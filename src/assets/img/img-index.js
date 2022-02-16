e72be6fc3773d2b76692d835ba9ef34e570ae104
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-16 04:22:01 -0500

    starter assets

---
+++ src/assets/img/img-index.js
@@ -0,0 +1,46 @@
+import EmailImg from './icons/email.svg?react'
+import FacebookImg from './icons/facebook.svg?react'
+import InstagramImg from './icons/instagram.svg?react'
+import LogoImg from './other/logo-no-border.svg'
+import HeroImg from './other/hero.jpeg'
+import FireGif from './fire.gif'
+
+import GalleryOne from './gallery/gallery-1.jpeg'
+import GalleryTwo from './gallery/gallery-2.jpeg'
+import GalleryThree from './gallery/gallery-3.jpeg'
+import GalleryFour from './gallery/gallery-4.jpeg'
+import GalleryFive from './gallery/gallery-5.jpeg'
+import GallerySix from './gallery/gallery-6.jpeg'
+import GallerySeven from './gallery/gallery-7.jpeg'
+import GalleryEight from './gallery/gallery-8.jpeg'
+import GalleryNine from './gallery/gallery-9.jpeg'
+import GalleryTen from './gallery/gallery-10.jpeg'
+import GalleryEleven from './gallery/gallery-11.jpeg'
+
+export const images = {
+  gallery: {
+    GalleryOne,
+    GalleryTwo,
+    GalleryThree,
+    GalleryFour,
+    GalleryFive,
+    GallerySix,
+    GallerySeven,
+    GalleryEight,
+    GalleryNine,
+    GalleryTen,
+    GalleryEleven,
+  },
+  other: {
+    LogoImg,
+    HeroImg,
+    FireGif,
+  },
+  icons: {
+    EmailImg,
+    FacebookImg,
+    InstagramImg,
+  },
+}
+
+export default images
