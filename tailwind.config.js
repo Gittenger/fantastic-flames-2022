0f986016d44a41493bf952ef7225aec0a7a9142c
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-16 04:16:36 -0500

    init and config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}', './public/index.html'],
+  theme: {
+    colors: {
+      white: '#fefefe',
+      gray: {
+        100: '#f8f8f8',
+        300: '#dddddd',
+        500: '#363435',
+        600: '#252324',
+        800: `#131213`,
+        900: '#0a0a0a',
+      },
+      red: {
+        300: '#eb3237',
+        800: '#a71318',
+      },
+    },
+    extend: {
+      fontFamily: {
+        body: ['Fira'],
+        brand: ['Game of Squids'],
+        nav: ['Ethnocentric'],
+        heading: ['Toxigenesis'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
