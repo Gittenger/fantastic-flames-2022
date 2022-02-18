6e0b9a13cf9fb5a751ed321f04c0743a54c83961
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-18 05:25:36 -0500

    lightbox provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 import './styles/index.css'
+import SimpleReactLightbox from 'simple-react-lightbox'
 import PageProvider from './contexts/PageProvider.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
     <PageProvider>
-      <App />
+      <SimpleReactLightbox>
+        <App />
+      </SimpleReactLightbox>
     </PageProvider>
   </React.StrictMode>,
   document.getElementById('root')
